a2673d52d81268934d58fc3979845813fad24eb9
Matt Brennan <mattyb1000@gmail.com>
2016-06-02 12:06:39 +0100

    remove debug, chdir into package dir to install and publish

--- src/publish.js
+++ src/publish.js
@@ -6,7 +6,13 @@ const publish = (...a) => promisify(origNpm.commands.publish)(...a);
 const install = (...a) => promisify(origNpm.commands.install)(...a);
 
 module.exports = async function(config, dir) {
-	await npm.loadAsync(config);
-	await install([]);
-	return publish([dir])
+	const oldDir = process.cwd();
+	process.chdir(dir);
+	try {
+		await npm.loadAsync(config);
+		await install([]);
+		return publish([dir])
+	} finally {
+		process.chdir(oldDir);
+	}
 };
