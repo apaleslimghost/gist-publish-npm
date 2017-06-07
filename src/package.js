92cda976f478c67b8a2e73aecf90830107e390aa
Matt Brennan <matt@153.io>
2017-06-07 19:05:09 +0100

    fetch latest versions of dependencies

--- src/package.js
+++ src/package.js
@@ -5,6 +5,8 @@ const HJSON = require('hjson');
 const merge = require('lodash.merge');
 const mapValues = require('lodash.mapvalues');
 const values = require('lodash.values');
+const promiseAllObject = require('@quarterto/promise-all-object');
+const latestVersion = require('latest-version');
 
 const actualPackageName = (req) => req.split('/', req[0] === '@' ? 2 : 1).join('/');
 
@@ -13,24 +15,27 @@ module.exports = async function({gistId, files, owner, history, description}) {
 	const fileContents = mapValues(files, ({content}) => content);
 	const main = filenames.find(name => name.endsWith('.js'));
 
+	const dependencies$ = promiseAllObject(values(fileContents)
+		.reduce((deps, content) => Object.assign(
+			deps,
+			detective(content)
+				.filter(p => !p.startsWith('.') && !builtins.includes(p))
+				.map(actualPackageName)
+				.reduce((obj, pack) => Object.assign(obj, {
+					[pack]: latestVersion(pack).then(version => `^${version}`)
+				}), {})
+		), {})
+	);
+
 	return merge({
 		main,
 		version: `1.${history.length - 1}.0`,
 		name: `@${owner.login}/${path.basename(main, '.js')}`,
 		bin: fileContents[main].startsWith('#!/usr/bin/env node') && main,
-		dependencies: values(fileContents)
-			.reduce((deps, content) => Object.assign(
-				deps,
-				detective(content)
-					.filter(p => !p.startsWith('.') && !builtins.includes(p))
-					.map(actualPackageName)
-					.reduce((obj, pack) => Object.assign(obj, {
-						[pack]: '*'
-					}), {})
-			), {}),
+		dependencies: await dependencies$,
 		repository: `gist:${gistId}`,
 		homepage: `https://gist.github.com/${gistId}`,
 
-		// author: 
+		// author:
 	}, HJSON.parse(description));
 };
